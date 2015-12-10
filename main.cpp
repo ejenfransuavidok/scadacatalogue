3111d06f1e0c14cb2142bef137d90d01e75bd30a
ejenfransuavidok <ejenfransuavidok@gmail.com>
2015-12-10 14:13:51 +0500

    work 1.1.a

--- main.cpp
+++ main.cpp
@@ -13,18 +13,6 @@ int main(int argc, char *argv[])
         qDebug() << *message;
         exit(0);
     }
-    /*
-    QXlsx::Document doc(QDir(QDir::currentPath()).filePath("test.xlsx"));
 
-    doc.addSheet("Каталог событий");
-
-    qDebug() << doc.read(1, 1).toString();
-    qDebug() << doc.read(2, 1).toString();
-    qDebug() << doc.read(3, 1).toString();
-
-    doc.save();
-
-    exit(0);
-    */
     return a.exec();
 }
